bae235856ec42b90461083e07897b1376c63f875
Boris Shminke <boris.shminke.ml>
2020-01-08 15:30:00 +0300

    ignore missing imports mypy error for some libs without stubs

--- show_report.sh
+++ show_report.sh
@@ -3,7 +3,7 @@
 PACKAGE_NAME=neural_semigroups
 pycodestyle ${PACKAGE_NAME} scripts tests
 pylint --rcfile=.pylintrc ${PACKAGE_NAME} scripts
-mypy ${PACKAGE_NAME} scripts tests
+mypy --config-file mypy.ini ${PACKAGE_NAME} scripts tests
 coverage run --source ${PACKAGE_NAME} -m unittest discover -s tests/
 coverage report -m
 cloc ${PACKAGE_NAME} scripts
