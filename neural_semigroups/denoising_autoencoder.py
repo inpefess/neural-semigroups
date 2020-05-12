7005cc7209ba37a90cdcc777c9d974ea873a590d
Boris Shminke <boris@shminke.ml>
2020-05-12 19:31:23 +0300

    add reparametrization trick

--- neural_semigroups/denoising_autoencoder.py
+++ neural_semigroups/denoising_autoencoder.py
@@ -24,31 +24,44 @@ from torch.nn.functional import dropout2d
 from neural_semigroups.constants import CURRENT_DEVICE
 
 
-def get_encoder_and_decoder_layers(dims: List[int]) -> Tuple[Module, Module]:
+def get_encoder_and_decoder_layers(
+    dims: List[int], split_last: bool
+) -> Tuple[Module, Module]:
     """
     construct symmetrical encoder and decoder modules
 
     :param dims: the dimensions of layers in an encoder part (the same dimensions are used for a decoder)
+    :param split_last: if ``True``, makes their last encoder dimensions twice larger than the first decoder dimension (for a reparameterization trick)
     :returns: a pair of two sequential models, representing encoder and decoder layers
     """
     encoder_layers: "OrderedDict[str, Module]" = OrderedDict()
     decoder_layers: "OrderedDict[str, Module]" = OrderedDict()
+    encoder_dims = dims.copy()
+    decoder_dims = list(reversed(dims))
+    if split_last:
+        encoder_dims[-1] *= 2
     for i in range(len(dims) - 1):
         encoder_layers.update(
-            {f"linear0{i}": Linear(dims[i], dims[i + 1], bias=True)}
+            {
+                f"linear0{i}": Linear(
+                    encoder_dims[i], encoder_dims[i + 1], bias=True
+                )
+            }
         )
         encoder_layers.update({f"relu0{i}": ReLU()})
-        encoder_layers.update({f"bn0{i}": BatchNorm1d(dims[i + 1])})
-        decoder_layers.update({f"bn1{i}": BatchNorm1d(dims[i])})
-        decoder_layers.update({f"relu1{i}": ReLU()})
+        encoder_layers.update({f"bn0{i}": BatchNorm1d(encoder_dims[i + 1])})
         decoder_layers.update(
-            {f"linear1{i}": Linear(dims[i + 1], dims[i], bias=True)}
+            {
+                f"linear1{i}": Linear(
+                    decoder_dims[i], decoder_dims[i + 1], bias=True
+                )
+            }
         )
+        decoder_layers.update({f"relu1{i}": ReLU()})
+        decoder_layers.update({f"bn1{i}": BatchNorm1d(decoder_dims[i + 1])})
     return (
-        Sequential(encoder_layers).to(CURRENT_DEVICE),
-        Sequential(OrderedDict(reversed(decoder_layers.items()))).to(
-            CURRENT_DEVICE
-        ),
+        Sequential(encoder_layers),
+        Sequential(decoder_layers),
     )
 
 
@@ -58,24 +71,30 @@ class MagmaDAE(Module):
     """
 
     apply_corruption: bool = True
-    do_sampling: bool = False
 
     def __init__(
-        self, cardinality: int, hidden_dims: List[int], corruption_rate: float
+        self,
+        cardinality: int,
+        hidden_dims: List[int],
+        corruption_rate: float,
+        do_reparametrization: bool = False,
     ):
         """
         :param cardinality: the number of elements in a magma
         :param hidden_dims: a list of sizes of hidden layers of the encoder and the decoder
         :param corruption_rate: what percentage of cells from the Cayley table to substitute with uniform random variables
+        :param do_reparametrization: if ``True``, adds a reparameterization trick
         """
         super().__init__()
         self.cardinality = cardinality
         self.corruption_rate = corruption_rate
-        self.input_dim = cardinality ** 3
+        self.do_reparametrization = do_reparametrization
         (
             self.encoder_layers,
             self.decoder_layers,
-        ) = get_encoder_and_decoder_layers(dims=[self.input_dim] + hidden_dims)
+        ) = get_encoder_and_decoder_layers(
+            [cardinality ** 3] + hidden_dims, do_reparametrization
+        )
         # pylint: disable=not-callable
         self._nearly_zero = torch.tensor([1e-6], device=CURRENT_DEVICE)
         # pylint: disable=not-callable
@@ -90,7 +109,9 @@ class MagmaDAE(Module):
         :param corrupted_input: a tensor with two indices
         :returns: some tensor with two indices and non-negative values
         """
-        return self.encoder_layers(corrupted_input.view(-1, self.input_dim))
+        return self.encoder_layers(
+            corrupted_input.view(corrupted_input.shape[0], -1)
+        )
 
     def corrupt_input(self, cayley_cube: Tensor) -> Tensor:
         """
@@ -145,7 +166,7 @@ class MagmaDAE(Module):
         :param mu_and_sigma: vector of expectation and standard deviation
         :returns: sample from a distribution
         """
-        if self.do_sampling:
+        if self.do_reparametrization:
             dim = mu_and_sigma.shape[1] // 2
             sample = torch.normal(mu_and_sigma[:, :dim], mu_and_sigma[:, dim:])
         else:
