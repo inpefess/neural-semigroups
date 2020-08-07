d9f5f474b8569b6463f8d4147cfbfcfe211a641d
Boris Shminke <boris@shminke.ml>
2020-08-07 20:19:56 +0300

    rise coverage threshold

--- show_report.sh
+++ show_report.sh
@@ -9,7 +9,7 @@ pycodestyle --max-doc-length 160 --ignore E203,E501,W503 \
 	    ${PACKAGE_NAME} scripts tests
 pylint --rcfile=.pylintrc ${PACKAGE_NAME} scripts
 mypy --config-file mypy.ini ${PACKAGE_NAME} scripts tests
-pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=91 \
+pytest --cov ${PACKAGE_NAME} --cov-report term-missing --cov-fail-under=92 \
        --junit-xml test-results/neural-semigroups.xml \
        --doctest-modules ${PACKAGE_NAME} tests/
 cloc ${PACKAGE_NAME}
