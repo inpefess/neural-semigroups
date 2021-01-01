610f19409306b528e8148f37ba2eef5812368735
Boris Shminke <boris@shminke.ml>
2021-01-01 11:55:51 +0100

    update copyright

--- scripts/generate_semigroups.py
+++ scripts/generate_semigroups.py
@@ -1,5 +1,5 @@
 """
-   Copyright 2019-2020 Boris Shminke
+   Copyright 2019-2021 Boris Shminke
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
